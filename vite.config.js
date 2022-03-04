8f23f134efb2d7af0bd42d1e58138ac2c61cf636
Marc-antoine simon <marc-antoine.simon3@orange.fr>
2022-03-04 17:57:53 +0100

    try 3 deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import vitePluginString from 'vite-plugin-string';
 
 export default {
     plugins: [
-        vitePluginString()
-    ]
+        vitePluginString(),
+    ],
+    base: '/earthPage/'
 }
